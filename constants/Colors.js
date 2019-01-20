679a4bb12c545ba83308c7443a11da1a9358545d
Issoufi Adam <issoufi.adam1@gmail.com>
2019-01-20 14:40:13 +0100

    first commit

---
+++ constants/Colors.js
@@ -0,0 +1,18 @@
+const tintColor = '#2f95dc';
+const primaryColor = 'rgb(131, 113, 187)';
+const primaryOppositeColor = '#FFFFFF';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  primaryColor,
+  primaryOppositeColor
+};
